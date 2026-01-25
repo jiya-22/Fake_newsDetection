2f423754eb30131dfbac893db9164dadb6de20ca
Shruti-369 <alewashruti@gmail.com>
2026-01-25 13:04:37 +0530

    Update App component

--- NoCap/src/App.jsx
+++ NoCap/src/App.jsx
@@ -1,15 +1,20 @@
-import NewsForm from "./components/NewsForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/NavBar.jsx";
+import Home from "./components/Home.jsx";
+import Detect from "./components/Detect.jsx";
 
 function App() {
   return (
-    <div className="app">
-      <h1>📰 Fake News Detection</h1>
-      <p>Paste news text below and check authenticity</p>
-      <NewsForm />
-      <h1 className="text-4xl font-bold text-pink-500">
-        Tailwind Working 🔥
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detect" element={<Detect />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
